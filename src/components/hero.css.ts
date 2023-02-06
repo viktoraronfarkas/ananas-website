38e20b0c6a793ba2cf2e1d1c093b10fe0fad880a
Viktor Farkas <viktoraron.farkas@gmail.com>
2023-02-06 16:21:50 +0100

    initial setup, experimenting

---
+++ src/components/hero.css.ts
@@ -0,0 +1,11 @@
+import { style } from "@vanilla-extract/css"
+import { theme } from "../theme.css"
+import { media } from "./ui.css"
+
+export const heroImageContainer = style({
+  textAlign: "center",
+})
+
+export const heroImage = style({
+  width: "200px",
+})
